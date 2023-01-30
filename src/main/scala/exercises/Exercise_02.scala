e5f5ee91fbe026142c960b9ceeee6e85b89f6bd8
[200~https://github.com/IbnuAlii~ <ibnuali600@gmail.com>
2023-01-30 20:50:02 +0300

    Exercise

--- src/main/scala/exercises/Exercise_02.scala
+++ src/main/scala/exercises/Exercise_02.scala
@@ -1,5 +1,41 @@
 package exercises
 
-object Exercise_02 {
+import scala.annotation.tailrec
 
+object Exercise_02 extends App {
+  /*
+  * 1. Concatenate a string n times
+  * 2. IsPrime function tail recursive
+  * 3. Fibonacci function, tail recursive
+  */
+
+//  * 1. Concatenate a string n times
+  @tailrec
+  def concatenateTailRec (aString: String, n: Int, accomulator: String): String =
+    if (n <= 0) accomulator
+    else concatenateTailRec(aString, n-1, aString + accomulator)
+
+  println(concatenateTailRec("Helo",3,""))
+
+//    * 2. IsPrime function tail recursive
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailRec (t: Int, isStillPrime: Boolean):Boolean =
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailRec(t - 1, n % t != 0 && isStillPrime)
+    isPrimeTailRec(n/2, true)
+  }
+  println(isPrime(2003))
+  println(isPrime(629))
+
+//    * 3. Fibonacci function, tail recursive
+def fibonacci(n: Int): Int = {
+  def fiboTailrec (i: Int, last: Int, nextToLast: Int): Int =
+    if (i >= n) last
+    else fiboTailrec(i + 1, last +  nextToLast, last)
+  if (n <= 2) 1
+  else fiboTailrec(2,1,1)
+}
+println(fibonacci(8))
 }
